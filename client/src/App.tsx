5b88b474344cf8ae3ecef7ffd39f1998be51ec96
Mizuha-hk <hk.haru0803@gmail.com>
2023-11-19 05:26:29 +0900

    [modify] temp commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
+import React from "react"
+import { Button, Divider, FluentProvider, Text, Title1, webLightTheme } from "@fluentui/react-components"
+import Header from "./components/header";
+
 function App() {
 
+  const [count, setCount] = React.useState(0);
+
   return (
-    <>
-      <h1>Home</h1>      
-    </>
+    <FluentProvider theme={webLightTheme}>
+      <Header />
+      <Divider />
+      <Title1>Counter</Title1>
+      <Button onClick={() => setCount(count + 1)}>Click me</Button>
+      <Text>{count}</Text>
+    </FluentProvider>  
   )
 }
 
