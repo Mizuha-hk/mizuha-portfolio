b7f35e88efdd54a4d1c6d0d0027ceb08db077040
Mizuha-hk <hk.haru0803@gmail.com>
2023-11-19 19:11:34 +0900

    [modify]  add style definition

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,9 +1,22 @@
-import { LargeTitle } from "@fluentui/react-components";
+import { Button, LargeTitle, makeStyles } from "@fluentui/react-components";
+
+const useStyles = makeStyles(
+  {
+    root:{
+      display:"flex",
+      alignItems:"center"
+    }
+  }
+)
 
 function Header() {
+  const style = useStyles();
   return (
-    <LargeTitle>Title</LargeTitle>
-);
+    <div className={style.root}>
+      <LargeTitle>Title</LargeTitle>
+      <Button as="a" href="/login">Log in</Button>
+    </div>
+  );
 }
 
 export default Header;
