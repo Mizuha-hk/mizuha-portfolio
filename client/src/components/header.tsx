0fed5e2dde6346c4e6877d4bae5f4a3c48a9ce08
Mizuha-hk <hk.haru0803@gmail.com>
2023-11-20 21:13:39 +0900

    temp commit

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,19 +1,33 @@
-import { Button, LargeTitle, makeStyles } from "@fluentui/react-components";
+import { Button, LargeTitle, Tab, TabList, makeStyles } from "@fluentui/react-components";
 
 const useStyles = makeStyles(
   {
     root:{
       display:"flex",
-      alignItems:"center"
-    }
+      alignItems:"center",
+      justifyContent:"space-between",
+    },
+    flexStart:
+    {
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"flex-start",
+    },
   }
-)
+);
 
 function Header() {
   const style = useStyles();
   return (
     <div className={style.root}>
-      <LargeTitle>Title</LargeTitle>
+      <div className={style.flexStart}>
+        <LargeTitle>Title</LargeTitle>
+        <TabList>
+          <Tab value="home">Home</Tab>
+          <Tab value="about">About</Tab>
+          <Tab value="contact">Contact</Tab>
+        </TabList>
+      </div>
       <Button as="a" href="/login">Log in</Button>
     </div>
   );
