b46dd2974deb57403ab45092de003950221d7772
Mizuha-hk <hk.haru0803@gmail.com>
2023-11-24 21:17:48 +0900

    [modify] #3 Adapt props

--- client/src/stories/AccountCard.stories.ts
+++ client/src/stories/AccountCard.stories.ts
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import AccountCard from "../components/AccountCard";
 
 const meta = {
-    title: 'components/accountCard',
+    title: 'components/AccountCard',
     component: AccountCard,
     tags: ['autodocs'],
     parameters: {
@@ -12,8 +12,22 @@ const meta = {
 } satisfies Meta<typeof AccountCard>;
 
 export default meta;
-// type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>;
 
-// export const LoggedIn: Story = {};
+export const LoggedIn: Story = {
+    args: {
+        isLogin: true,
+        username: 'UserName',
+        url: 'https://github.com/microsoft',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/6154722?s=200&v=4',
+    }
+};
 
-// export const LoggedOut: Story = {};
+export const LoggedOut: Story = {
+    args: {
+        isLogin: false,
+        username: 'UserName',
+        url: '',
+        avatarUrl: '',
+    }
+};
